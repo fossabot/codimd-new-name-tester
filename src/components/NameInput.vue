2ba8856ff7f6992bccebf23dc793f59874d8dced
Erik Michelson <github@erik.michelson.eu>
2020-06-10 13:56:12 +0200

    Added mobile support

--- src/components/NameInput.vue
+++ src/components/NameInput.vue
@@ -1,11 +1,16 @@
 <template>
-  <input
-    class="name"
-    type="text"
-    autofocus
-    @keyup="changeName"
-    placeholder="CodiMD"
-  >
+  <form @submit.prevent="submitName">
+    <input
+      class="name"
+      type="text"
+      autofocus
+      @keyup="changeName"
+      placeholder="CodiMD"
+    >
+    <button
+      type="submit"
+    ><i class="fa fa-search"></i></button>
+  </form>
 </template>
 
 <script>
@@ -14,28 +19,47 @@ export default {
   methods: {
     changeName (event) {
       this.$emit('change', event.target.value)
-      if (event.code === 'Enter') {
-        this.$emit('check')
-      }
+    },
+    submitName () {
+      this.$emit('check')
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
+form {
+  display: flex;
+  flex-flow: row nowrap;
+}
+
+@mixin heightMargin {
+  height: 3.5rem;
+  margin: 1.5rem 0;
+}
+
 input.name {
+  @include heightMargin;
   display: block;
   width: 100%;
-  height: 3.5rem;
   font-size: 2rem;
-  border-radius: 8px;
+  border-radius: 8px 0 0 8px;
   border: none;
-  margin: 1.5rem 0;
   padding: 0 0.5rem;
 
   &:focus {
     outline: none;
     box-shadow: #111 0 0 12px 1px;
   }
+}
+
+button {
+  @include heightMargin;
+  cursor: pointer;
+  background-color: #ffffff;
+  border: none;
+  border-radius: 0 8px 8px 0;
+  font-size: 1.5rem;
+  padding: 0 1.5rem;
 }
 </style>
